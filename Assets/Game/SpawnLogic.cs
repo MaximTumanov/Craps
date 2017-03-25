0d87992a9562406664fc001f1e971f453514b09f
Fadij Isber <fadij.isber@murka.com>
2017-03-25 12:23:43 +0200

    Dies

---
+++ Assets/Game/SpawnLogic.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRStandardAssets.Utils;
+public class SpawnLogic : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject prefab;
+
+    [SerializeField]
+    private VRInput VRi;
+
+    ThrowLogic tr;
+
+    GameObject obj;
+    float t;
+
+    void Start()
+    {
+        VRi.OnDown += VRi_OnDown;
+        VRi.OnUp += VRi_OnUp;
+    }
+
+    private void VRi_OnUp()
+    {
+        
+        tr.Disepower = ((Time.time - t) * 1000f) % 1200f;
+
+        obj.SetActive(true);
+    }
+
+    private void VRi_OnDown()
+    {
+      //  if (Input.GetMouseButtonDown(0))
+        {
+            obj = Instantiate(prefab, transform.position, Quaternion.identity) as GameObject;
+
+            tr = obj.GetComponent<ThrowLogic>();
+
+            t = Time.time;
+        }
+    }
+
+
+}
---
+++ Assets/Game/ThrowLogic.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowLogic : MonoBehaviour {
+
+    [SerializeField]
+    private Rigidbody Dise; 
+    public float Disepower;
+
+    void Start()
+    {        
+        Throw(Disepower);
+    }
+
+    private void Throw(float powerOfThrow)
+    {
+      
+            Dise.useGravity = true;
+        
+            Dise.AddForce(Vector3.forward * powerOfThrow);
+        
+    }
+
+   
+}
