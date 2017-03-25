736dc52aa162d7be0207f177126bbf813104c86f
Sokolov Anton <sokolov.anton@murka.com>
2017-03-25 14:34:37 +0200

    [SL +] gameLogic

---
+++ Assets/Scripts/Core/Game/Bet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Bet
+{
+    public int Id;
+    public int Amount;
+
+    public Bet(int id, int amount)
+    {
+        Id = id;
+        Amount = amount;
+    }
+}
---
+++ Assets/Scripts/Core/Game/Table.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Table : MonoBehaviour
+{
+    public Player[] Players;
+    public Shooter Shooter;
+    public GamePhase CurrentGamePhase;
+
+    public void PayOut(int dieResult)
+    {
+    
+    }
+
+    public void PayOut()
+    {
+        
+    }
+}
