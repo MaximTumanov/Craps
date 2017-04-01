100a2222c37c59b1c5d4de2d000b9d08fce64e66
Yaroslav Khodakovskiy <jaroslav.khodakovskiy@murka.com>
2017-04-01 18:00:04 +0300

    Added bet areas prefab and functionality

---
+++ Assets/Scripts/Core/Game/BetAreaOnTable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class BetAreaOnTable : MonoBehaviour
+{
+    public static string[] PossibleBetNames = new string[] { "Pass", "DontPass", "Come", "DontCome", "Field", "Seven", "AnyCraps",
+        "Point4", "Point5", "Point6", "Point8", "Point9", "Point10",
+        "Hardway22", "Hardway33", "Hardway44", "Hardway55", "Hardway11", "Hardway66", "Hardway12", "Hardway56",
+        "Big6", "Big8" };
+    [HideInInspector] public string BetName;
+   
+}
